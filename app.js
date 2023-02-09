d3ca0b82f02cb75306d9b8321dc73b0be9cf0c50
thao <littlelili0994@gmail.com>
2023-02-09 17:09:23 +0700

    Dinh Nghia Cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+// Khai báo router
+const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +11,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+
+// Tạo api để gọi
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
